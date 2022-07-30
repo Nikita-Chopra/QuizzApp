53965cdd268d73a98566c022017ad42ba2220f7f
Nikita Chopra <nikitachopra11@gmail.com>
2022-07-30 01:44:45 -0700

    msg

---
+++ src/components/Start.js
@@ -0,0 +1,27 @@
+import { useRef } from "react";
+
+export default function Start({setUserName}) {
+
+    const inputRef = useRef();
+
+const handleClick =()=>{
+     inputRef.current.value && setUserName(inputRef.current.value);
+}
+  return (
+    <>
+    <div className='start-a'>
+    <div className="start">
+    <input
+      className="startInput"
+      placeholder="enter your name"
+      ref={inputRef}
+    />
+    <button className="startButton" onClick={handleClick}>
+      Start
+    </button>
+  </div>
+  </div>
+    </>
+
+  )
+}
